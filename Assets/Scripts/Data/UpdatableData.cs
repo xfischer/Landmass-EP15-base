a529547943c0fd84b5ddfd2bde9e050c9debe708
Xavier Fischer <xavier@fischer.ninja>
2016-11-12 10:25:45 +0100

    initial checkin

---
+++ Assets/Scripts/Data/UpdatableData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdatableData : ScriptableObject {
+
+	public event System.Action OnValuesUpdated;
+
+	public bool autoUpdate;
+
+	protected virtual void OnValidate() {
+		if (autoUpdate) {
+			NotifyOnUpdatedValues();
+		}
+	}
+
+	public void NotifyOnUpdatedValues() {
+		if (OnValuesUpdated != null			) {
+			OnValuesUpdated();
+		}
+	}
+
+
+}
+ 
