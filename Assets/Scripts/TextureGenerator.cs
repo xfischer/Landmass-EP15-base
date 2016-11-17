52b18112a1561aeba1362dfdcbd3e9e9ea25c7c1
Xavier Fischer <xavier.fischer@outlook.com>
2016-11-17 15:18:29 +0100

    texture aniso and filtering

--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -5,7 +5,9 @@ public static class TextureGenerator {
 
 	public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height) {
 		Texture2D texture = new Texture2D (width, height);
-		texture.filterMode = FilterMode.Point;
+		texture.filterMode = FilterMode.Bilinear;
+		//texture.filterMode = FilterMode.Point	;
+		texture.anisoLevel = 6;
 		texture.wrapMode = TextureWrapMode.Clamp;
 		texture.SetPixels (colourMap);
 		texture.Apply ();
